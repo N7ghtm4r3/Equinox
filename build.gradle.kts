566dc09b65bc7ea03a6654d13e56ae9b7f558702
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2024-10-08 21:35:14 +0200

    Set the com.vanniktech.maven.publish gradle plugin to migrate the publication to Maven Central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.KotlinJvm
+import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.dokka.DokkaConfiguration.Visibility.*
 import org.jetbrains.dokka.base.DokkaBase
 import org.jetbrains.dokka.base.DokkaBaseConfiguration
@@ -5,9 +8,9 @@ import org.jetbrains.dokka.gradle.DokkaTask
 
 plugins {
     id("java")
-    id("maven-publish")
     kotlin("jvm")
     id("org.jetbrains.dokka") version "1.9.20"
+    id("com.vanniktech.maven.publish") version "0.29.0"
 }
 
 buildscript {
@@ -38,17 +41,44 @@ dependencies {
     implementation("commons-validator:commons-validator:1.7")
 }
 
-afterEvaluate {
-    publishing {
-        publications {
-            create<MavenPublication>("maven") {
-                groupId = "com.tecknobit.equinox"
-                artifactId = "Equinox"
-                version = "1.0.4"
-                from(components["java"])
+mavenPublishing {
+    configure(
+        platform = KotlinJvm(
+            javadocJar = JavadocJar.Dokka("dokkaHtml"),
+            sourcesJar = true
+        )
+    )
+    coordinates(
+        groupId = "io.github.n7ghtm4r3",
+        artifactId = "Equinox",
+        version = "1.0.4"
+    )
+    pom {
+        name.set("Equinox")
+        description.set("Utilities for backend services based on Springboot framework. Is a support library to implement some utilities both for backend and for client also who comunicate with that Springboot backend")
+        inceptionYear.set("2024")
+        url.set("https://github.com/N7ghtm4r3/Equinox")
+
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://opensource.org/license/mit")
             }
         }
+        developers {
+            developer {
+                id.set("N7ghtm4r3")
+                name.set("Manuel Maurizio")
+                email.set("maurizio.manuel2003@gmail.com")
+                url.set("https://github.com/N7ghtm4r3")
+            }
+        }
+        scm {
+            url.set("https://github.com/N7ghtm4r3/Equinox")
+        }
     }
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
 }
 
 kotlin {
